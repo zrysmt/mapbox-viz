5511daa7930733f7732814bc28a9c9b81f106384
zrysmt <742311374@qq.com>
2017-06-06 14:48:30 +0800

    update

--- app/component/basemap.js
+++ app/component/basemap.js
@@ -1,12 +1,21 @@
 import mapboxgl from 'mapbox-gl';
 
 const token = "pk.eyJ1IjoidGVjaGZlIiwiYSI6ImNqMHVrMmt1cDA0Y2czMm10dWlsb3UzcmEifQ.B28sl4Ds0bQKD706bgdzUg";
+let map;
 
-mapboxgl.accessToken = token;
+class BaseMap {
+    init() {
 
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    center:[104,30],
-    zoom:4
-});
+        mapboxgl.accessToken = token;
+
+        map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v9',
+            center: [104, 30],
+            zoom: 4
+        });
+        this.map = map;
+    }
+}
+
+export {BaseMap,map};
--- app/index.js
+++ app/index.js
@@ -1,8 +1,36 @@
 import './common/css/index.scss';
 
 import util from './common/util.js';
-import {map} from './component/basemap.js';
+// import stats from './common/libs/stats.js';
+import Stats from 'stats.js';
+
+import { BaseMap, map } from './component/basemap.js';
 
 
 
-util.adaptHeight("map",0);
+function init() {
+    let baseMap = new BaseMap();
+    baseMap.init();
+    util.adaptHeight("map", 0);
+}
+
+init();
+
+//show fps
+let stats = new Stats();
+stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild(stats.dom);
+
+function animate() {
+
+    stats.begin();
+
+    // monitored code goes here
+
+    stats.end();
+
+    requestAnimationFrame(animate);
+
+}
+
+requestAnimationFrame(animate);
